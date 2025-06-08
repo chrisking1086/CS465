daa8274580531abf9ddd86f65b4d04e5fd611a4c
Chris King <chris.king1086@gmail.com>
2025-06-08 14:48:02 -0500

    Module 5 completed baseline

--- travlr/app_api/models/db.js
+++ travlr/app_api/models/db.js
@@ -14,9 +14,11 @@ const connect = () => {
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${dbURI}`);
 });
+
 mongoose.connection.on('error', err => {
     console.log('Mongoose connection error: ', err);
 });
+
 mongoose.connection.on('disconnected', () => {
     console.log('Mongoose disconnected');
 });
---
+++ travlr/app_server/controllers/rooms.js
@@ -0,0 +1,8 @@
+/* GET rooms view */
+const rooms = (req, res) => {
+    res.render('rooms', { title: 'Rooms' });
+};
+
+module.exports = {
+    rooms
+};
