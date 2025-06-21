eb3d0cf2895e26469b12f249631a485072f058d5
Chris King <chris.king1086@gmail.com>
2025-06-21 11:50:28 -0500

    Module 7 completed baseline

--- travlr/app.js
+++ travlr/app.js
@@ -20,6 +20,13 @@ var apiRouter = require('./app_api/routes/index');
 // Bring in the database
 require('./app_api/models/db');
 
+// Bring in our enviornment file, enviornment variable capabilities
+require('dotenv').config();
+
+// Wire in our authentication module
+var passport = require('passport');
+require('./app_api/config/passport');
+
 var app = express();
 
 // view engine setup
@@ -35,11 +42,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message });
+  }
+});
+
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -56,14 +74,14 @@ app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 // import { trips } from '../data/trip';
 import { TripCardComponent } from '../trip-card/trip-card.component';
+import { AuthenticationService } from '../services/authentication.service';
+
 
 import { Trip } from '../models/trip';
 import { TripDataService } from '../services/trip-data.service';
@@ -10,43 +12,49 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [ CommonModule, TripCardComponent ],
+  imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css',
   providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
   // trips: Array<any> = trips;
-    trips!: Trip[];
-    message: string = ''; 
+  trips!: Trip[];
+  message: string = '';
 
   constructor(private tripDataService: TripDataService,
-              private router: Router) {
+    private router: Router,
+    private authenticationService: AuthenticationService) {
     console.log('trip-listing constructor');
   }
 
-  public addTrip(): void{
+  public addTrip(): void {
     this.router.navigate(['add-trip']);
   }
 
   private getStuff(): void {
     this.tripDataService.getTrips()
-    .subscribe({
-      next: (value: any) => {
-        this.trips = value;
-        if(value.length > 0){
-          this.message = 'There are ' + value.length + ' trips available.';
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if (value.length > 0) {
+            this.message = 'There are ' + value.length + ' trips available.';
+          }
+          else {
+            this.message = 'There were no trips retireved from the database';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
         }
-      else{
-        this.message = 'There were no trips retireved from the database';
-      }
-      console.log(this.message);
-      },
-      error: (error: any) => {
-        console.log('Error: ' + error);
-      }
-    })
-}
+      })
+  }
+
+  public isLoggedIn() {
+    return this.authenticationService.isLoggedIn();
+  }
+
 
   ngOnInit(): void {
     console.log('ngOnInit');
